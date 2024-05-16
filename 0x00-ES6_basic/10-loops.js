2653e2d3a40776ea83ad466d6383322331ad3c45
Fadzaiwendymuzadzi <fadzaiwendym@gmail.com>
2024-05-16 16:03:43 -0700

    all tasks

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-	  const array2 = [];
-	  for (const value of array) {
-		      array2.push(appendString + value);
-		    }
-	  return array2;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+  return newArray;
 }
